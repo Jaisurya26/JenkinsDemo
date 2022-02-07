0ee6bfc7ed3963aca36162619c3b974c42c5aa6c
Jaisurya26 <85210721+Jaisurya26@users.noreply.github.com>
2022-02-07 15:23:23 +0530

    Update HelloGuys.java

--- HelloGuys.java
+++ HelloGuys.java
@@ -3,8 +3,7 @@ public class HelloGuys {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Jaisurya");
-		System.out.println("I am tester");
-		System.out.println("I am an automation tester");
+		System.out.println("I am a automation tester");
 				System.out.println("sdhdhdfdfjjj");
 
 		
